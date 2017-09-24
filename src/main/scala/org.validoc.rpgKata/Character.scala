1b3138bcf8718f01bee0ebcd9eadb92906b2f880
Phil <phil.rice@iee.org>
2017-09-24 13:34:18 +0200

    Working with type class now

--- src/main/scala/org.validoc.rpgKata/Character.scala
+++ src/main/scala/org.validoc.rpgKata/Character.scala
@@ -1,6 +1,7 @@
 package org.validoc.rpgKata
 
 import java.util.concurrent.atomic.AtomicInteger
+import Utilities._
 
 case class Faction(name: String)
 
@@ -8,16 +9,50 @@ case class Level(l: Int) {
   def isFiveOrMoreHigher(other: Level) = l - 5 >= other.l
 }
 
-case class Character(name: String, faction: Faction, range: Range = Melee, level: Level = Level(1), alive: LiveStatus = Alive, hitPoints: HitPoints = HitPoints(1000)) {
+case class Attack[Att: CanFight, Def: CanFight](attacker: Att, defender: Def, hitPoints: HitPoints, distance: Meters) {
 
-  def damage(attacker: Character, distance: Meters, hitPoints: HitPoints): Character = ???
+  import CanFight._
+
+  val realDamage = (attacker.level, defender.level) match {
+    case (l1, l2) if l1 isFiveOrMoreHigher l2 => hitPoints.plusFiftyPercent
+    case (l1, l2) if l2 isFiveOrMoreHigher l1 => hitPoints.fiftyPercent
+    case _ => hitPoints.asIs
+  }
+
+  def damage: Def = {
+    if ((attacker != this) && (attacker.range.canHit(distance)) && attacker.faction != defender.faction) {
+      if (hitPoints.lessThanZero) defender else {
+        val newHitPoints = this.defender.hitPoints - realDamage
+        println(s"Hitpoints $hitPoints, $realDamage, $newHitPoints")
+        if (newHitPoints.lessThanZero) defender.kill else defender.setHitPoints(newHitPoints)
+      }
+    } else defender
+  }
 }
-
 
 object Character {
   val damageCount = new AtomicInteger()
 
+  implicit object CanFightForCharacter extends CanFight[Character] {
+    override def range(t: Character) = t.range
+
+    override def faction(t: Character) = t.faction
+
+    override def level(t: Character) = t.level
+
+    override def kill(t: Character) = t.copy(alive = Dead, hitPoints=HitPoints(0))
+
+    override def setHitPoints(t: Character, hitPoints: HitPoints) = t.copy(hitPoints = hitPoints)
+
+    override def hitPoints(t: Character) = t.hitPoints
+  }
+
 }
+
+case class Character(name: String, faction: Faction, range: Range = Melee, level: Level = Level(1), alive: LiveStatus = Alive, hitPoints: HitPoints = HitPoints(1000)) {
+  def damage(attacker: Character, distance: Meters, hitPoints: HitPoints): Character = new Attack[Character, Character](attacker, this, hitPoints, distance).damage
+}
+
 
 object Scenario extends App {
   val someAttacker = Character("Attacker", Faction("NPCs"))
